06e11f24c2cec19a51619894d38d445baf193a24
Felipe030628 <andresq0128@gmail.com>
2026-07-24 11:49:00 -0500

    Cambio de Conexion

--- src/java/Controlador/Conexion.java
+++ src/java/Controlador/Conexion.java
@@ -5,15 +5,16 @@ import java.sql.DriverManager;
 
 public class Conexion {
     Connection con;
-    // Datos de tu base de datos (asegúrate de que el nombre sea bd_barstock o el tuyo)
-    String url = "jdbc:mysql://localhost:3307/Barstock"; 
+    // Datos de tu base de datos en Railway
+    String url = "jdbc:mysql://sakura.proxy.rlwy.net:24908/railway?useSSL=false&serverTimezone=UTC"; 
     String user = "root";
-    String pass = ""; // Tu contraseña de MySQL
+    String pass = "aSZLfvKfJVnchDkGkHFWxnycCmAICyJU";
 
     public Connection Conexion() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             con = DriverManager.getConnection(url, user, pass);
+            System.out.println("¡Conexión exitosa a la base de datos de Railway!");
         } catch (Exception e) {
             System.err.println("Error de conexión: " + e);
         }
